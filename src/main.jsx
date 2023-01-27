d57cb2e1c7aba914c18d1c5475c19b85b2367f35
Rafael <joaquiingm@gmail.com>
2023-01-27 07:35:06 +0100

    Primer commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { GifExpertApp } from './GifExpertApp';
+import './styles.css';
+
+
+//'8Wb4TNvyJmUBgBlt49uo1TpEUtExuPor';
+ReactDOM.createRoot(document.getElementById('root')).render(
+ <React.StrictMode>
+    <GifExpertApp/>
+  </React.StrictMode>,
+)
